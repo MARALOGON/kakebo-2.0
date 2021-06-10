a51ca36cfa9a3cff2e7e1f00ad6a90c907fb7ebf
Marcos Alonso <marcosalonso@MBP-de-Marcos.home>
2021-06-10 22:36:56 +0200

    Muestra movimientos

---
+++ kakebo/views.py
@@ -0,0 +1,23 @@
+from flask.json import jsonify
+from kakebo import app
+from kakebo.dataaccess import DBmanager
+from flask import jsonify, render_template
+import sqlite3
+
+
+dbManager = DBmanager(app.config.get('DATABASE'))
+
+@app.route('/')
+def listaMovimientos():
+    return render_template('spa.html')
+
+
+@app.route('/api/v1/movimientos')
+def movimientosAPI():
+    query = "SELECT * FROM movimientos order by fecha;"
+    
+    try:
+        lista = dbManager.consultaMuchasSQL(query)
+        return jsonify({'status': 'success', 'movimientos':lista})
+    except sqlite3.Error as e:
+        return jsonify({'status': 'fail', 'mensaje': str(e)})
